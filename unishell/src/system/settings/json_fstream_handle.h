f3936835a4b9632c980ebe73235ec485456ab2c6
Marvin Marjan <marvin.marjanc@hotmail.com>
2023-06-28 10:56:07 -0400

    added "JSON to Literal" converter and "Literal to JSON"

--- unishell/src/system/settings/json_fstream_handle.h
+++ unishell/src/system/settings/json_fstream_handle.h
@@ -15,7 +15,7 @@ public:
 		std::string path;
 	};
 
-	JSONFstreamHandle() {}
+	JSONFstreamHandle() : fail_(false) {}
 	JSONFstreamHandle(const std::string& path) 
 		: path_(path)
 	{
@@ -31,6 +31,8 @@ public:
 		if (data.is_string()) return new lit::LiteralValue(data.get<std::string>());
 		if (data.is_number()) return new lit::LiteralValue(data.get<double>());
 		if (data.is_boolean()) return new lit::LiteralValue(data.get<bool>());
+		if (data.is_array()) return JSONListToLiteralValueList(data);
+		if (data.is_object()) return JSONObjectToLiteralValueObject(data);
 
 		return nullptr;
 	}
@@ -39,9 +41,54 @@ public:
 		if (value->index() == 0) return asStr(value);
 		if (value->index() == 1) return asDbl(value);
 		if (value->index() == 2) return asBool(value);
+		if (value->index() == 3) return LiteralValueListToJSONValue(value);
+		if (value->index() == 4) return LiteralValueObjectToJSONValue(value);
 
-		return nullptr;
+		return json();
 	}
+
+
+
+	static json LiteralValueListToJSONValue(lit::LiteralValue* value) {
+		lit::LitList list = asList(value);
+		json jVal;
+		
+		for (lit::LiteralValue* item : list)
+			jVal.push_back(LiteralValueToJSONValue(item));
+
+		return jVal;
+	}
+
+	static json LiteralValueObjectToJSONValue(lit::LiteralValue* value) {
+		lit::LitObject obj = asObj(value);
+		json jVal;
+
+		for (const auto& [key, value] : obj)
+			jVal[key] = LiteralValueToJSONValue(value);
+
+		return jVal;
+	}
+
+
+
+	static lit::LiteralValue* JSONListToLiteralValueList(json jVal) {
+		lit::LitList list;
+
+		for (const auto& val : jVal)
+			list.push_back(JSONValueToLiteralValue(val));
+
+		return lit::lit(list);
+	}
+
+	static lit::LiteralValue* JSONObjectToLiteralValueObject(json jVal) {
+		lit::LitObject obj;
+
+		for (const auto& [key, value] : jVal.items())
+			obj.insert({ key, JSONValueToLiteralValue(value) });
+
+		return lit::lit(obj);
+	}
+
 
 
 	void load() {
